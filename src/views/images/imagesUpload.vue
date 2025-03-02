51ecf6e1740b45975411fac7e06c6b4a9705f7d4
少女の梦 <2652511934@qq.com>
2025-03-02 19:52:31 +0800

    feat:添加轻提示

--- src/views/images/imagesUpload.vue
+++ src/views/images/imagesUpload.vue
@@ -7,8 +7,8 @@
       :on-preview="handlePictureCardPreview"
       class="uploadClass"
       drag
-      action="http://127.0.0.1:3000/api/upload/"
       :headers
+      action="http://127.0.0.1:3000/api/upload/"
       accept=".jpg,.png,.gif,.jpeg"
       :limit="10"
       list-type="picture"
@@ -30,6 +30,7 @@
 import { UploadFilled } from "@element-plus/icons-vue";
 import { useAuthStore } from "@/stores/user"; // 引入 Pinia 的 store
 import { ref } from "vue";
+import { ElMessage } from "element-plus";
 // 文件上传请求头
 const authStore = useAuthStore(); // 获取 Pinia 中的 auth store
 const token = authStore.token; // 从 Pinia 中获取 token
@@ -38,15 +39,25 @@ const headers = {
 };
 // 文件上传成功的钩子
 const onSuccess = async (file, files) => {
-  console.log("上传成功", file, files);
+  // console.log("上传成功", file, files);
+  ElMessage({
+    message: "上传成功!",
+    type: "success",
+    plain: true,
+  });
 };
 // 文件状态改变时的钩子，添加文件、上传成功和上传失败时都会被调用
 const onChange = async (file, files) => {
-  console.log("文件状态改变", file, files);
+  // console.log("文件状态改变", file, files);
 };
 // 文件上传失败的钩子
 const onError = async (file, files) => {
-  console.log("上传失败", file, files);
+  // console.log("上传失败", file, files);
+  ElMessage({
+    message: file.message,
+    type: "error",
+    plain: true,
+  });
 };
 // 文件图片列表预览
 const fileList = ref([]);
