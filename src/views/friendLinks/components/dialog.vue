ad90d21fdc78ac7563820addab1e448af39cee63
少女の梦 <2652511934@qq.com>
2025-03-11 19:57:05 +0800

    feat:新增友链渲染

--- src/views/friendLinks/components/dialog.vue
+++ src/views/friendLinks/components/dialog.vue
@@ -1,45 +1,100 @@
 <template>
   <!-- 展示组件 -->
   <div>
-    <el-dialog
-      v-model="dialogVisible"
-      title="Tips"
-      width="500"
-      :before-close="handleClose"
-    >
-      <span>This is a message</span>
+    <el-dialog v-model="dialogVisible" :title="prop.title" width="500">
+      <div class="form">
+        <el-form :inline="true" :model="formInline" class="form-inline">
+          <el-form-item label="友链名称">
+            <el-input
+              v-model="formInline.username"
+              placeholder="请输入友链名称"
+              clearable
+            />
+          </el-form-item>
+          <el-form-item label="友链地址">
+            <el-input
+              v-model="formInline.url"
+              placeholder="请输入友链地址"
+              clearable
+            />
+          </el-form-item>
+          <el-form-item label="友链描述">
+            <el-input
+              v-model="formInline.description"
+              placeholder="请输入友链描述"
+              clearable
+            />
+          </el-form-item>
+        </el-form>
+      </div>
       <template #footer>
         <div class="dialog-footer">
-          <el-button @click="onChangeState(false)">Cancel</el-button>
-          <el-button type="primary" @click="onChangeState(false)">
-            Confirm
-          </el-button>
+          <el-button @click="onChangeState(false)">取消</el-button>
+          <el-button type="primary" @click="submit"> 确认 </el-button>
         </div>
       </template>
     </el-dialog>
   </div>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
-import { ElMessageBox } from "element-plus";
-
+import { reactive, ref } from "vue";
+import linkApi from "@/apis/links.js";
+import { ElMessage } from "element-plus";
+// 组件状态
 const dialogVisible = ref(false);
+// 组件标题
+const prop = defineProps(["title"]);
 
+// 表单
+const formInline = reactive({
+  username: "",
+  url: "",
+  description: "",
+});
+// 清除表单方法
+const clearForm = () => {
+  Object.assign(formInline, {
+    username: "",
+    url: "",
+    description: "",
+  });
+};
+// 提交表单
+const submit = async () => {
+  // 添加友链
+  if (prop.title === "添加友链") {
+    const res = await linkApi.addlink(formInline);
+    ElMessage.success("添加友链成功");
+  }
+  // 更新友链
+  else if (prop.title === "更新友链") {
+    const res = await linkApi.updateLink({
+      ...formInline,
+    });
+    ElMessage.success("更新友链成功");
+  }
+  clearForm();
+  onChangeState(false);
+};
 // 改变组件的状态
 const onChangeState = (state = false) => {
   dialogVisible.value = state;
 };
+// 数据回显
+const formData = data => {
+  Object.assign(formInline, data);
+};
 // 暴露修改状态的方法给父组件
 defineExpose({
   onChangeState,
+  submit,
+  formData,
 });
-const handleClose = (done: () => void) => {
-  ElMessageBox.confirm("Are you sure to close this dialog?")
-    .then(() => {
-      done();
-    })
-    .catch(() => {
-      // catch error
-    });
-};
 </script>
+<style scope>
+.form-inline {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+</style>
--- src/views/friendLinks/friendLinks.vue
+++ src/views/friendLinks/friendLinks.vue
@@ -1,46 +1,46 @@
 <script setup>
 import { ElMessage } from "element-plus";
 import Dialog from "./components/dialog.vue";
-import { ref } from "vue";
-const tableData = [
-  {
-    date: "2016-05-03",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-02",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-04",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-01",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-];
+import linkAPI from "@/apis/links.js";
+import { onMounted, ref } from "vue";
 // 搜索输入
 const input = ref("");
+// 展示组件名
+const title = ref("");
 // 添加友链的组件状态
 const dialogVisible = ref(false);
 // 搜索友链
 const onSearch = async () => {
   ElMessage.success("搜索触发成功");
 };
+// 获取友链信息
+const links = ref();
+const getLinkData = async () => {
+  const res = await linkAPI.getLinks();
+  links.value = res.data.result;
+  console.log(links.value);
+};
+onMounted(() => {
+  getLinkData();
+});
 // 添加触发成功
 const onAddLink = async () => {
   ElMessage.success("添加触发成功");
 };
+// 获取子组件
 const dialogRef = ref();
 // 打开添加友链成功
 const onOpenAddLink = async () => {
-  ElMessage.success("打开添加友链成功");
   dialogVisible.value = true;
+  title.value = "添加友链";
+  dialogRef.value.onChangeState(dialogVisible.value);
+};
+// 更新友链
+const onUpdateLink = row => {
+  dialogVisible.value = true;
+  title.value = "更新友链";
+  // 父传子数据回显
+  dialogRef.value.formData(row);
   dialogRef.value.onChangeState(dialogVisible.value);
 };
 // 分页组件改变时
@@ -51,7 +51,7 @@ const onChange = async (page, pageSize) => {
 <template>
   <div class="link">
     <!-- 展示组件 -->
-    <Dialog ref="dialogRef" />
+    <Dialog :title="title" ref="dialogRef" />
     <!-- 表格头 -->
     <div class="header">
       <!-- 搜索 -->
@@ -74,13 +74,15 @@ const onChange = async (page, pageSize) => {
     <!-- 表格 -->
     <div class="main">
       <div class="table">
-        <el-table :data="tableData" border style="width: 100%">
-          <el-table-column prop="date" label="Date" width="180" />
-          <el-table-column prop="name" label="Name" width="180" />
-          <el-table-column prop="address" label="Address" />
+        <el-table :data="links" border style="width: 100%">
+          <el-table-column prop="username" label="友链名称" width="180" />
+          <el-table-column prop="url" label="友链链接" width="180" />
+          <el-table-column prop="description" label="友链描述" />
           <el-table-column fixed="right" label="操作" width="160">
             <template #default="{ row }">
-              <el-button type="primary">修改</el-button>
+              <el-button @click="onUpdateLink(row)" type="primary"
+                >修改</el-button
+              >
               <el-popconfirm
                 @confirm="onRemoveImage(row)"
                 @cancel="onCancel()"
